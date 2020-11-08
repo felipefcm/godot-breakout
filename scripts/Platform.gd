158649e5820a60b1b83f10120b871e2a2e8d7074
Felipe Martins <ffcm@outlook.com>
2020-11-08 22:29:26 +1300

    Ditching platform physics logic

--- scripts/Platform.gd
+++ scripts/Platform.gd
@@ -1,22 +1,24 @@
 extends KinematicBody2D
 class_name Platform
 
-export (float) var maxSpeed = 80;
-export (float) var maxAcceleration = 10;
-export (float) var drag = 50;
+export (float) var maxSpeed = 500;
+export (float) var maxAcceleration = 100;
+export (float) var frictionCoefficient = 0.1;
+export (float) var inputAcceleration = 50;
 
 var velocity: Vector2 = Vector2.ZERO;
 var acceleration: Vector2 = Vector2.ZERO;
 
-func _process(delta):
+func _physics_process(delta):
 
 	var leftStr = Input.get_action_strength('ui_left');
 	var rightStr = Input.get_action_strength('ui_right');
 
-	acceleration += Vector2(rightStr - leftStr, 0).clamped(maxAcceleration) * delta;
-	var dragForce = (velocity * -1).normalized() * drag;
+	var motionForce = Vector2(rightStr - leftStr, 0) * inputAcceleration * delta;
+	var dragForce = -velocity * frictionCoefficient * delta;
 
-func _physics_process(delta):
-	
-	velocity += velocity + acceleration;
-	move_and_collide((velocity * delta).normalized() * maxSpeed);
+	acceleration = (acceleration + motionForce + dragForce).clamped(maxAcceleration);
+	velocity = (velocity + acceleration).clamped(maxSpeed);
+
+	print('Velocity ', velocity, ' acceleration ', acceleration);
+	move_and_collide(velocity * delta);
